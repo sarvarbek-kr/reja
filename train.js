26cc91a43cb9639eb4d9daa428e9c9ae55b11c94
Walter <sarvarbek200028@gmail.com>
2024-08-21 01:22:49 +0900

    feat: train callback hamda ASYN functions

--- train.js
+++ train.js
@@ -7,26 +7,68 @@ const list = [
     "yoshlarga investitsiya qiling",           //50-60
     "endi dam oling, foydasi yo'q endi",   // 60
 ];
-
+// CALLBACK function
 function maslahatBering(a, callback) {
-    if(typeof a !== 'number') callback ("please insert a number", null);
+    if (typeof a !== "number") callback("Insert a number", null);
     else if (a <= 20) callback(null, list[0]);
-    else if (a > 20 && a <= 30) callback(null, list[1]);
-    else if (a > 30 && a <= 40) callback(null, list[2]);
-    else if (a > 40 && a <= 50) callback(null, list[3]);
-    else if (a > 50 && a <= 60) callback(null, list[4]);
-   else {
-    setTimeout( function () {
-         callback(null, list[5]);
-    }, 5000);
-   }
+    else if (a < 20 && a <= 30) callback(null, list[1]);
+    else if (a < 20 && a <= 30) callback(null, list[2]);
+    else if (a < 20 && a <= 30) callback(null, list[3]);
+    else if (a < 20 && a <= 30) callback(null, list[4]);
+    else {
+        setInterval(function () {
+            callback(null, list[5]);
+        }, 1000);
+    }
 }
 
+console.log("passed here 0");
+maslahatBering(70, (err, data) => {
+    if (err) console.log("ERROR:", err);
+    else {
+        console.log(data);
+    }
+});
+console.log("passed here 1");
 
-console.log ("passed here 0")
-maslahatBering(65, (err, data) => {
-    if (err) console.log("error:", err);
-    else{
-    console.log("javob:", data);}
-}); 
-console.log("passed here 1")
+
+// ASYNC FUNCTION
+// async function maslahatBering(a) {
+//     if(typeof a !== 'number') throw new Error ("please insert a number");
+//     else if (a <= 20) return list[0];
+//     else if (a > 20 && a <= 30) return list[1];
+//     else if (a > 30 && a <= 40) return list[2];
+//     else if (a > 40 && a <= 50) return list[3];
+//     else if (a > 50 && a <= 60) return list[4];
+//    else {
+//     return new Promise((resolve, reject) => {
+//         // setTimeout(() => {
+//             setInterval(() => {
+//             resolve(list[5]);  
+//         }, 1000);
+//     });
+//    }
+// }
+ 
+// // CALL VIA then & catch
+
+// console.log ("passed here 0")
+// maslahatBering(25)
+// .then(data => {
+//     console.log("javob:", data);
+// }).catch(err => {
+//     console.log("error:", err);
+// });
+// console.log("passed here 1");
+
+
+//  asyn/ await
+// async function run() {
+//    let javob = await maslahatBering(65);
+//    console.log(javob);
+//    javob = await maslahatBering(71);
+//    console.log (javob);
+//    javob = await maslahatBering(41);
+//    console.log(javob);
+// }
+// run();
