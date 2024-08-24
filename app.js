e34ccc646d4ef07eb03a4190c2c6bede832496d2
Walter <sarvarbek200028@gmail.com>
2024-08-25 02:08:05 +0900

    feat: frontEnd javaScriptni qurish

--- app.js
+++ app.js
@@ -24,13 +24,8 @@ app.post("/create-item", (req, res) => {
     console.log("user entered / create-items");
    const new_reja = req.body.reja;
    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-    if(err) {
-        console.log(err);
-        console.log("===bu 30 line");
-        res.end("something went wrong");
-    } else {
-        res.end("successfully added");
-    }
+    console.log(data.ops);
+    res.json(data.ops[0]);
    });
 });
 
